9bb7578cd091e96212a74ad2959bb247a28fa06f
Preslava7 <preslavayordanova1409@gmail.com>
2023-02-07 12:52:47 +0200

    add project

---
+++ ObrabotkanaMasiv/ObrabotkanaMasiv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObrabotkanaMasiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
